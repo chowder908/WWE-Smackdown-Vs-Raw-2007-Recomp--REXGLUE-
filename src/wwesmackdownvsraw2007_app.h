7f98b06c2227bd3ea439c108ab3d0f4831701c74
chowder908 <chowder90856@gmail.com>
2026-05-21 05:46:20 -0500

    Update wwesmackdownvsraw2007_app.h

--- src/wwesmackdownvsraw2007_app.h
+++ src/wwesmackdownvsraw2007_app.h
@@ -5,6 +5,7 @@
 #pragma once
 
 #include <rex/rex_app.h>
+#include <rex/filesystem/devices/host_path_device.h>
 
 class Wwesmackdownvsraw2007App : public rex::ReXApp {
  public:
@@ -16,10 +17,36 @@ class Wwesmackdownvsraw2007App : public rex::ReXApp {
         PPCImageConfig));
   }
 
+
 void OnConfigurePaths(rex::PathConfig& paths) override {
     std::filesystem::path exe_dir = std::filesystem::current_path();
     paths.game_data_root = exe_dir / "assets";
+    paths.user_data_root = exe_dir / "saves";
+    paths.cache_root = exe_dir / "cache";
   }
+
+void OnPostSetup() override {
+    window()->SetTitle("WWE SmackDown vs. Raw 2007: ReXGlue");
+    auto exe_dir = std::filesystem::current_path();
+    auto cache = exe_dir / "cache";
+    auto saves = exe_dir / "saves";
+    std::filesystem::create_directories(cache);
+    std::filesystem::create_directories(saves);
+    auto* vfs = runtime()->file_system();
+
+    auto cache_device = std::make_unique<rex::filesystem::HostPathDevice>(
+        "\\Device\\Cache", cache, false);
+    cache_device->Initialize();
+    vfs->RegisterDevice(std::move(cache_device));
+    vfs->RegisterSymbolicLink("cache:", "\\Device\\Cache");
+
+    auto profile_device = std::make_unique<rex::filesystem::HostPathDevice>(
+        "\\Device\\Profile0", saves, false);
+    profile_device->Initialize();
+    vfs->RegisterDevice(std::move(profile_device));
+    vfs->RegisterSymbolicLink("profile0:", "\\Device\\Profile0");
+}
+};
 
   // Override virtual hooks for customization:
   // void OnPostInitLogging() override {}
@@ -29,4 +56,3 @@ void OnConfigurePaths(rex::PathConfig& paths) override {
   // void OnCreateDialogs(rex::ui::ImGuiDrawer* drawer) override {}
   // void OnShutdown() override {}
   // void OnConfigurePaths(rex::PathConfig& paths) override {}
-};
